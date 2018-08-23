f5420672f2221f77c8c87177ff92be94af909281
chenjun <chenjun@molitv.cn>
2018-08-23 14:23:50 +0800

    latest api/core/v1

--- baremetalcp/loadbalancer.go
+++ baremetalcp/loadbalancer.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/golang/glog"
 
+	"k8s.io/api/core/v1"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	apiv1 "k8s.io/client-go/pkg/api/v1"
-	"k8s.io/kubernetes/pkg/api/v1"
 	"k8s.io/kubernetes/pkg/cloudprovider"
 )
 
