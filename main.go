769c72956026c60894293463c599231ec60a90d5
chenjun <chenjun@molitv.cn>
2018-08-27 17:16:34 +0800

    refer to https://kubernetes.io/docs/tasks/administer-cluster/developing-cloud-controller-manager/#developing

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
+	goflag "flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
-	"github.com/golang/glog"
-	"github.com/spf13/pflag"
-
-	"k8s.io/apiserver/pkg/server/healthz"
-	"k8s.io/apiserver/pkg/util/flag"
+	utilflag "k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
 	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
-	"k8s.io/kubernetes/cmd/cloud-controller-manager/app/options"
 	_ "k8s.io/kubernetes/pkg/client/metrics/prometheus" // for client metric registration
-	"k8s.io/kubernetes/pkg/cloudprovider"
+	// NOTE: Importing all in-tree cloud-providers is not required when
+	// implementing an out-of-tree cloud-provider.
 	_ "k8s.io/kubernetes/pkg/cloudprovider/providers"
 	_ "k8s.io/kubernetes/pkg/version/prometheus" // for version metric registration
 
-	"github.com/chongzii6/cloud-provider-baremetal/baremetalcp"
+	"github.com/spf13/pflag"
+	"k8s.io/apiserver/pkg/server/healthz"
 )
 
 func init() {
@@ -25,24 +25,43 @@ func init() {
 }
 
 func main() {
-	s := options.NewCloudControllerManagerServer()
-	s.AddFlags(pflag.CommandLine)
-	addVersionFlag()
+	// s := options.NewCloudControllerManagerServer()
+	// s.AddFlags(pflag.CommandLine)
+	// addVersionFlag()
 
-	flag.InitFlags()
+	// flag.InitFlags()
+	// logs.InitLogs()
+	// defer logs.FlushLogs()
+
+	// printAndExitIfRequested()
+
+	// cloud, err := cloudprovider.InitCloudProvider(baremetalcp.ProviderName, s.CloudConfigFile)
+	// if err != nil {
+	// 	glog.Fatalf("Cloud provider could not be initialized: %v", err)
+	// }
+
+	// glog.Info("Starting version ", version)
+	// if err := app.Run(s, cloud); err != nil {
+	// 	fmt.Fprintf(os.Stderr, "%v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	command := app.NewCloudControllerManagerCommand()
+
+	// TODO: once we switch everything over to Cobra commands, we can go back to calling
+	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
+	// normalize func and add the go flag set by hand.
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	// utilflag.InitFlags()
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
-	printAndExitIfRequested()
-
-	cloud, err := cloudprovider.InitCloudProvider(baremetalcp.ProviderName, s.CloudConfigFile)
-	if err != nil {
-		glog.Fatalf("Cloud provider could not be initialized: %v", err)
-	}
-
-	glog.Info("Starting version ", version)
-	if err := app.Run(s, cloud); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
+
 }
