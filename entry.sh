8344dfbf11b9ddcb0c9a9742fe6936f4b49d0619
chongzii6 <juncn@hotmail.com>
2018-08-28 04:17:18 -0400

    entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 echo -- start git pull
 cd /go/src/github.com/chongzii6/cloud-provider-baremetal
+
+git config --global user.email "chenjun@molitv.cn"
+git config --global user.name "chongzii6"
+
 git pull
 
 echo -- go build
